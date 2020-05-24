097dc44dc539dbe4618160e928a1a72746534cc6
BrianV1981 <48861936+BrianV1981@users.noreply.github.com>
2020-05-24 13:33:17 -0400

    Update aliveStoreMenu.hpp

--- modules/aliveStoreMenu.hpp
+++ modules/aliveStoreMenu.hpp
@@ -11,7 +11,7 @@
                 description = "Purchasing this will give you 30 minutes of access to a RAH-66 Comanchie combat support helicoptor, that can be utilized from your Ace Self Interact Menu > ALiVE > ALiVE Combat Support.";
                 price = 75000;
                 stock = 100;
-		code = [player,"scripts\CAS1.sqf"] call BIS_fnc_execVM;
+		code = _handle = [] execVM "scripts\CAS1.sqf";
             };
 			class B_Plane_CAS_01_F {
                 displayName = "ALiVE A-10D Thunderbolt II Combat Support (30 Minutes)";
@@ -34,6 +34,33 @@
                 stock = 100;
 		code = [player,"scripts\CAS4.sqf"] call BIS_fnc_execVM;
             };
-			
+			class B_MBT_01_arty_F {
+                displayName = "**BROKEN**ALiVE Sholef Combat Support (30 Minutes)";
+                description = "**BROKEN**DO NOT BUY**This Sholef combat support artillery can be utilized from your Ace Self Interact Menu > ALiVE > ALiVE Combat Support.";
+                price = 50000;
+                stock = 100;
+		code = [player,"ARTY1.sqf"] call BIS_fnc_execVM;
+            };
+			class B_Heli_Light_01_F {
+                displayName = "**BROKEN**ALiVE MH-9 Hummingbird Transport (30 Minutes)";
+                description = "**BROKEN**DO NOT BUY**Purchasing this will give you 30 minutes of access to a MH-9 Hummingbird transport helicoptor, that can be utilized from your Ace Self Interact Menu > ALiVE > ALiVE Combat Support.";
+                price = 20000;
+                stock = 100;
+		code = _handle = [] execVM "scripts\CAS1.sqf";
+            };
+			class B_Heli_Transport_01_F {
+                displayName = "**BROKEN**ALiVE UH-80 Ghost Hawk Transport (30 Minutes)";
+                description = "**BROKEN**DO NOT BUY**Purchasing this will give you 30 minutes of access to a UH-80 Ghost Hawk transport helicoptor, that can be utilized from your Ace Self Interact Menu > ALiVE > ALiVE Combat Support.";
+                price = 50000;
+                stock = 100;
+		code = [player,"TRANSPORT1.sqf"] call BIS_fnc_execVM;
+            };
+			class B_Heli_Transport_03_F {
+                displayName = "**BROKEN**ALiVE CH-67 Huron Transport (30 Minutes)";
+                description = "**BROKEN**DO NOT BUY**Purchasing this will give you 30 minutes of access to a CH-67 Huron transport helicoptor, that can be utilized from your Ace Self Interact Menu > ALiVE > ALiVE Combat Support.";
+                price = 100000;
+                stock = 100;
+		code = [player,"TRANSPORT2.sqf"] call BIS_fnc_execVM;
+            };
 	};			
 };
