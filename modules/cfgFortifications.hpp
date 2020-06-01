f87b20274f746903560c3ddd9f01ce02cf477a97
BrianV1981 <48861936+BrianV1981@users.noreply.github.com>
2020-05-31 22:04:30 -0400

    Add files via upload

--- modules/cfgFortifications.hpp
+++ modules/cfgFortifications.hpp
@@ -171,10 +171,6 @@ class CfgGradFortifications {
 		
 		class Land_HelipadCircle_F {
             size = 1;
-        };		
-
-		class Land_HelipadCircle_F {
-            size = 1;
         };
 		
 		class Land_Cargo_Tower_V1_F {
