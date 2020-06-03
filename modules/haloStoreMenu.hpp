b00f68e51cb237eeafba8b400333df6a668c5929
BrianV1981 <48861936+BrianV1981@users.noreply.github.com>
2020-06-03 16:51:14 -0400

    Update haloStoreMenu.hpp

--- modules/haloStoreMenu.hpp
+++ modules/haloStoreMenu.hpp
@@ -7,15 +7,15 @@
             kindOf = "Other";
 
 			class B_T_VTOL_01_infantry_F {
-                displayName = "HALO/HAHO Group Jump";
-                description = "In military operations, HALO is also used for delivering equipment, supplies, or personnel, while HAHO is generally used exclusively for personnel. In typical HALO/HAHO insertions the troops jump from altitudes between 15,000 feet (4,600 m) and 35,000 feet (11,000 m).";
+                displayName = "HALO/HAHO Solo Jump";
+                description = "**WARNING**When exiting the store, your map will autmatically be opened. If you left click on the map you will be HALO dropped at that location! You can save your HALO drop until the next time that you left click on the map!***In military operations, HALO is also used for delivering equipment, supplies, or personnel, while HAHO is generally used exclusively for personnel.";
                 price = 5000;
                 stock = 100;
 		code = "if (player == (_this select 0)) then {execVM 'scripts\MGI_HALOcustom_1.sqf'}";
             };
 			class B_T_VTOL_01_vehicle_F {
                 displayName = "HALO/HAHO Group Jump";
-                description = "In military operations, HALO is also used for delivering equipment, supplies, or personnel, while HAHO is generally used exclusively for personnel. In typical HALO/HAHO insertions the troops jump from altitudes between 15,000 feet (4,600 m) and 35,000 feet (11,000 m).";
+                description = "**WARNING**When exiting the store, your map will autmatically be opened. If you left click on the map grouped AI units and yourself will be HALO dropped at that location! You can save your group HALO drop until the next time that you left click on the map!***In military operations, HALO is also used for delivering equipment, supplies, or personnel, while HAHO is generally used exclusively for personnel.";
                 price = 15000;
                 stock = 100;
 		code = "if (player == (_this select 0)) then {execVM 'scripts\MGI_HALOcustom_2.sqf'}";
