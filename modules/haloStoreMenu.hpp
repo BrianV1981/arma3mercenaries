bd700c9a1dd1ed9120294428019bf534ea2e0d17
BrianV1981 <48861936+BrianV1981@users.noreply.github.com>
2020-06-03 13:50:59 -0400

    Update haloStoreMenu.hpp

--- modules/haloStoreMenu.hpp
+++ modules/haloStoreMenu.hpp
@@ -6,14 +6,14 @@
             displayName = "HAHO/HALO Jump";
             kindOf = "Other";
 
-			class Land_HelipadCircle_F {
+			class B_T_VTOL_01_infantry_F {
                 displayName = "HALO/HAHO Group Jump";
                 description = "In military operations, HALO is also used for delivering equipment, supplies, or personnel, while HAHO is generally used exclusively for personnel. In typical HALO/HAHO insertions the troops jump from altitudes between 15,000 feet (4,600 m) and 35,000 feet (11,000 m).";
                 price = 5000;
                 stock = 100;
 		code = "if (player == (_this select 0)) then {execVM 'scripts\MGI_HALOcustom_1.sqf'}";
             };
-			class Land_HelipadCircle_F {
+			class B_T_VTOL_01_vehicle_F {
                 displayName = "HALO/HAHO Group Jump";
                 description = "In military operations, HALO is also used for delivering equipment, supplies, or personnel, while HAHO is generally used exclusively for personnel. In typical HALO/HAHO insertions the troops jump from altitudes between 15,000 feet (4,600 m) and 35,000 feet (11,000 m).";
                 price = 15000;
